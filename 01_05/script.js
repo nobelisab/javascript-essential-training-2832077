742210359ab7465ab3badc58ab8e93fc7292a71c
nobelisab <36314923+nobelisab@users.noreply.github.com>
2022-01-23 20:34:31 -0500

    Update script.js

--- 01_05/script.js
+++ 01_05/script.js
@@ -7,50 +7,54 @@
 /* Multi-line comment
 See! this line is also commented out! */
 
-const updateBackpack = (update) => {
+const updateInvestment = (update) => {
   let main = document.querySelector("main"); // main is an element
-  main.innerHTML = markup(backpack);
+  main.innerHTML = markup(investment);
   console.info(update);
 };
 
-const backpack = {
-  name: "Everyday Backpack",
-  volume: 30,
-  color: "grey",
-  pocketNum: 15,
-  strapLength: {
-    left: 26,
-    right: 26,
-  },
-  lidOpen: false,
-  toggleLid: function (lidStatus) {
-    this.lidOpen = lidStatus;
-    updateBackpack(`Lid status changed.`);
-  },
-  newStrapLength: function (lengthLeft, lengthRight) {
-    this.strapLength.left = lengthLeft;
-    this.strapLength.right = lengthRight;
-    updateBackpack(`Strap lengths updated.`);
-  },
+const investment = {
+  name: "Apple",
+  ticker:  "AAPL",
+  price: "",
+  portfolio:  "ETF";
 };
 
-const markup = (backpack) => {
+const ETF = {
+   stock1: "",
+   stock2: "",
+   bond1: "",
+   bond2: "";
+};
+
+const client = {
+   name: "",
+   address: "",
+   email: "",
+   phone: "";
+};
+
+const risklevel = {
+   riskscale number = "";
+};
+
+const advisor = {
+   name: "Elisabeth Noble";
+};
+
+const markup = (investment) => {
   return `
   <div>
-	<h3>${backpack.name}</h3>
+	<h3>${investment.name}</h3>
 	<ul>
-	  <li>Volume: ${backpack.volume}</li>
-	  <li>Color: ${backpack.color}</li>
-	  <li>Number of pockets: ${backpack.pocketNum}</li>
-	  <li>Strap lengths: L: ${backpack.strapLength.left}, R: ${
-    backpack.strapLength.right
-  } </li>
-	  <li>Top lid: ${backpack.lidOpen ? "Open" : "Closed"}</li>
+	  <li>Ticker: ${investment.ticker}</li>
+	  <li>Price: ${investment.price}</li>
+	  <li>Portfolio: ${investment.portfolio}</li>
 	</ul>
   </div>
 `;
 };
 
 const main = document.createElement("main");
-main.innerHTML = markup(backpack);
+main.innerHTML = markup(investment);
 document.body.appendChild(main);
